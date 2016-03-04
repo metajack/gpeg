883254591bc4f5ff50f9969c840bde70db152eb7
Jack Moffitt <jack@metajack.im>
2016-03-04 15:53:55 -0700

    Add handling for escape key.

--- src/main.rs
+++ src/main.rs
@@ -304,6 +304,10 @@ fn main() {
         for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return,
+                glium::glutin::Event::KeyboardInput(
+                    glium::glutin::ElementState::Released,
+                    _,
+                    Some(glium::glutin::VirtualKeyCode::Escape)) => return,
                 _ => (),
             }
         }
